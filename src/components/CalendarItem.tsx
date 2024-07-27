0aab9d9bd93c5a6a447b80bb57cd2556cd6e608f
Sune.Lianes <sune.lianes@student.uib.no>
2024-07-27 22:33:50 +0200

    added things

--- src/components/CalendarItem.tsx
+++ src/components/CalendarItem.tsx
@@ -1,7 +1,21 @@
-type CalendarItem = {
+import {Text, View} from "react-native";
+
+type CalendarItemProps = {
     subjectCode: string,
     start: Date,
     end: Date,
     location: string,
-    eventType: string,
+    eventType: CalenderEvent,
+}
+
+export const CalendarItem = (props: CalendarItemProps) => {
+    const { subjectCode, start, end, location, eventType } = props
+    return (
+        <View>
+            <Text>{subjectCode}</Text>
+            <Text>{eventType.toString()}</Text>
+            <Text>{start.getTime()} - {end.getTime()}</Text>
+            <Text>{location}</Text>
+        </View>
+    )
 }
