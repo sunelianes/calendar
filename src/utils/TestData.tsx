009660dc26c50434510540378f04c8e147fd8138
Sune.Lianes <sune.lianes@student.uib.no>
2024-07-27 22:33:38 +0200

    testdata file

---
+++ src/utils/TestData.tsx
@@ -0,0 +1,12 @@
+import {CalendarItem} from "../components/CalendarItem";
+
+
+
+export const INF102 =
+     <CalendarItem
+        subjectCode={"INF102"}
+        start={new Date()}
+        end={new Date()}
+        location={"RFB"}
+        eventType={CalenderEvent.Lecture}
+     >
