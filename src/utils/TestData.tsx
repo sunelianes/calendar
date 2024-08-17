c9f4669785f5cdf2d41886a9c76ed878abe0eaee
Sune.Lianes <sune.lianes@student.uib.no>
2024-08-17 10:45:12 +0200

    updated testdata for more testdata

--- src/utils/TestData.tsx
+++ src/utils/TestData.tsx
@@ -1,12 +1,28 @@
-import {CalendarItem} from "../components/CalendarItem";
+
+import {CalenderEvent} from "./CalenderEvent";
 
 
 
-export const INF102 =
-     <CalendarItem
-        subjectCode={"INF102"}
-        start={new Date()}
-        end={new Date()}
-        location={"RFB"}
-        eventType={CalenderEvent.Lecture}
-     >
+export const TestData = [
+    {
+         subjectCode: "INF102",
+         start: new Date(),
+         end: new Date(),
+         location: "RFB",
+         eventType: CalenderEvent.Lecture,
+    },
+    {
+         subjectCode: "INF113",
+         start: new Date(),
+         end: new Date(),
+         location: "Hoytek",
+         eventType: CalenderEvent.Lecture,
+    },
+    {
+         subjectCode: "INF122",
+         start: new Date(),
+         end: new Date(),
+         location: "A66",
+         eventType: CalenderEvent.Lecture,
+    },
+]
