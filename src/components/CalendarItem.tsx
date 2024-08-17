02bf1786b653ab49119b77a821ca09ac5aa168f0
Sune.Lianes <sune.lianes@student.uib.no>
2024-08-17 10:46:36 +0200

    updated CalendarItem.tsx

--- src/components/CalendarItem.tsx
+++ src/components/CalendarItem.tsx
@@ -1,4 +1,6 @@
-import {Text, View} from "react-native";
+import {StyleSheet, Text, View} from "react-native";
+import React from "react";
+import {CalenderEvent} from "../utils/CalenderEvent";
 
 type CalendarItemProps = {
     subjectCode: string,
@@ -8,14 +10,30 @@ type CalendarItemProps = {
     eventType: CalenderEvent,
 }
 
-export const CalendarItem = (props: CalendarItemProps) => {
+export const CalendarItem: React.FC<CalendarItemProps> = (props: CalendarItemProps) => {
     const { subjectCode, start, end, location, eventType } = props
     return (
-        <View>
-            <Text>{subjectCode}</Text>
-            <Text>{eventType.toString()}</Text>
+        <View style={styles.item}>
+            <Text style={styles.header}>{start.toDateString()}.</Text>
+            <Text>{subjectCode} {eventType.toString()}</Text>
             <Text>{start.getTime()} - {end.getTime()}</Text>
             <Text>{location}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        borderRadius: 20,
+    },
+    header: {
+        fontSize: 20,
+        alignSelf: "center",
+        backgroundColor: "red",
+        width: "100%",
+    }
+})
